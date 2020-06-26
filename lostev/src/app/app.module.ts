5f43b59351bfca3408db3639dd65da29ea49d010
Uroš Vujošević <UrosVuj@users.noreply.github.com>
2020-06-26 21:03:30 +0200

    Vreated map component

--- lostev/src/app/app.module.ts
+++ lostev/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { FormsModule } from "@angular/forms";
+import { MapComponent } from './map/map.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, MapComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
