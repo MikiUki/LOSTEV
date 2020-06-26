374c1074cb952701e6fcc80daf59c35f2fdb59d4
Uroš Vujošević <UrosVuj@users.noreply.github.com>
2020-06-27 01:48:55 +0200

    Map finished, needs CSS touchup

--- lostev/src/app/app.module.ts
+++ lostev/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { GoogleMapsModule } from '@angular/google-maps'
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -7,10 +8,11 @@ import { LoginComponent } from "./login/login.component";
 import { FormsModule } from "@angular/forms";
 import { MapComponent } from './map/map.component';
 
+
 @NgModule({
   declarations: [AppComponent, LoginComponent, MapComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, GoogleMapsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- lostev/src/app/map/map.component.ts
+++ lostev/src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GoogleMap, GoogleMapsModule, MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap
+  infoContent = ""
+
   constructor() { }
 
-  ngOnInit(): void {
+  markers: any[];
+  center: google.maps.LatLngLiteral;
+
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+
   }
+
+
+
+  ngOnInit(): void {
+
+    this.center = {
+      lat: 44.805503,
+      lng: 20.476134
+    }
+
+    this.markers = [
+      {
+        position: new google.maps.LatLng(44.805503, 20.476134),
+        title: "Marker 1",
+        label: { text: "E", color: 'orange' },
+        options: { animation: google.maps.Animation.DROP },
+        info: "Event: Heart attack" + '<br/>' + "Age: 65" + "<br/>" + "Gender: Male"
+      },
+      {
+        position: new google.maps.LatLng(44.805503, 20.47),
+        title: "Marker 2",
+        label: { text: "F", color: 'brown' },
+        options: { animation: google.maps.Animation.DROP },
+        info: "Event: Fire inside apartment building" + '<br/>' + "Number of people: 3+" + '<br/>' + "Floors: 3-5"
+      },
+      {
+        position: new google.maps.LatLng(44.8, 20.48),
+        title: "Marker 3",
+        label: { text: "P", color: 'blue' },
+        options: { animation: google.maps.Animation.DROP },
+        info: "Event: Riot in the streets" + '<br/>' + "Number of people: 500+" + '<br/>' + "Violent: Yes"
+      }
+
+    ];
+
+
+  }
+
+  openInfo(marker: google.maps.Marker, content, markerelem: MapMarker) {
+    this.infoContent = content;
+    this.infoWindow.open(markerelem)
+  }
+
+  submit() {
+
+  }
+
+
 
 }
