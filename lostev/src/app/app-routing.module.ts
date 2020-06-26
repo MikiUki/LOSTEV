543cbc256e495678980a760c9ce9d834ce928b9b
Uroš Vujošević <UrosVuj@users.noreply.github.com>
2020-06-26 15:43:39 +0200

    Login init, route setup

--- lostev/src/app/app-routing.module.ts
+++ lostev/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "login", component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
