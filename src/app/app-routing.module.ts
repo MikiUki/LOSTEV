187c117459c5660a645c3ebe8ad863fcfa8ed0ec
Uroš Vujošević <UrosVuj@users.noreply.github.com>
2020-06-27 21:59:36 +0200

    user landing page finished (srpski)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { MapComponent } from './map/map.component';
 import { SubmitComponent } from './submit/submit.component';
+import { DetaljiComponent } from './detalji/detalji.component';
+import { KorisnikComponent } from './korisnik/korisnik.component';
 
 const routes: Routes = [
   { path: "", component: LoginComponent },
   { path: "login", component: LoginComponent },
-  { path: "user", component: LoginComponent },
   { path: "dispatcher", component: MapComponent },
-  { path: "submit", component: SubmitComponent }
+  { path: "submit", component: SubmitComponent },
+  { path: "user", component: KorisnikComponent },
+  { path: "detalji", component: DetaljiComponent }
 
 ];
 
---
+++ src/app/korisnik/korisnik.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-korisnik',
+  templateUrl: './korisnik.component.html',
+  styleUrls: ['./korisnik.component.css']
+})
+export class KorisnikComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
