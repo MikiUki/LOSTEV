d58b392750eb923c4a26c2cc5f49d96397ab5dc0
Uroš Vujošević <UrosVuj@users.noreply.github.com>
2020-06-27 23:27:36 +0200

    user side finished and cleaned up

---
+++ detalji/detalji.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalji',
+  templateUrl: './detalji.component.html',
+  styleUrls: ['./detalji.component.css']
+})
+export class DetaljiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
