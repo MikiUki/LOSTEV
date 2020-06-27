3d9d3765c9acbeee3fb6d3aa66ae54953e48ab61
Uroš Vujošević <UrosVuj@users.noreply.github.com>
2020-06-27 09:58:17 +0200

    added responders to the map

--- lostev/src/app/map/map.component.ts
+++ lostev/src/app/map/map.component.ts
@@ -39,21 +39,43 @@ export class MapComponent implements OnInit {
         position: new google.maps.LatLng(44.805503, 20.476134),
         title: "Marker 1",
         label: { text: "E", color: 'orange' },
-        options: { animation: google.maps.Animation.DROP },
+        options: { animation: google.maps.Animation.BOUNCE },
         info: "Event: Heart attack" + '<br/>' + "Age: 65" + "<br/>" + "Gender: Male"
       },
       {
-        position: new google.maps.LatLng(44.805503, 20.47),
-        title: "Marker 2",
-        label: { text: "F", color: 'brown' },
+        position: new google.maps.LatLng(44.811516, 20.478889),
+        title: "Ambulance Palilula",
+        label: { text: "A", color: 'brown' },
         options: { animation: google.maps.Animation.DROP },
-        info: "Event: Fire inside apartment building" + '<br/>' + "Number of people: 3+" + '<br/>' + "Floors: 3-5"
+        info: "Ambulance" + '<br/>' + "Number of available vehicles: 4" + '<br/>'
       },
       {
-        position: new google.maps.LatLng(44.8, 20.48),
+        position: new google.maps.LatLng(44.800152, 20.480334),
         title: "Marker 3",
         label: { text: "P", color: 'blue' },
         options: { animation: google.maps.Animation.DROP },
+        info: "Police Department" + '<br/>' + "Number of available people: 34"
+      },
+
+      {
+        position: new google.maps.LatLng(44.811975, 20.487802),
+        title: "Fire Department",
+        label: { text: "F", color: 'brown' },
+        options: { animation: google.maps.Animation.DROP },
+        info: "Fire department" + '<br/>' + "Number of available vehicles: 6"
+      },
+      {
+        position: new google.maps.LatLng(44.805503, 20.48),
+        title: "Marker 2",
+        label: { text: "E", color: 'brown' },
+        options: { animation: google.maps.Animation.BOUNCE },
+        info: "Event: Fire inside apartment building" + '<br/>' + "Number of people: 3+" + '<br/>' + "Floors: 3-5"
+      },
+      {
+        position: new google.maps.LatLng(44.803758, 20.467646),
+        title: "Marker 3",
+        label: { text: "E", color: 'blue' },
+        options: { animation: google.maps.Animation.BOUNCE },
         info: "Event: Riot in the streets" + '<br/>' + "Number of people: 500+" + '<br/>' + "Violent: Yes"
       }
 
